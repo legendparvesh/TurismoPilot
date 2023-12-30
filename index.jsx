570b4306758fcc2b4e81cbfc5a3dac7ade1f12f0
legendparvesh <79099310+legendparvesh@users.noreply.github.com>
2023-12-30 10:18:05 +0530

    website creation
    web update

---
+++ index.jsx
@@ -0,0 +1,208 @@
+import React from "react";
+import "./style.css";
+
+export const MacbookAir = () => {
+  return (
+    <div className="macbook-air">
+      <div className="div">
+        <div className="overlap">
+          <div className="overlap-group">
+            <div className="overlap-2">
+              <div className="left-side" />
+              <div className="text-wrapper">Dashboard</div>
+              <div className="text-wrapper-2">Dashboard</div>
+              <div className="text-wrapper-3">Transfer</div>
+              <div className="text-wrapper-4">Analytics</div>
+              <div className="text-wrapper-5">Savings</div>
+              <div className="text-wrapper-6">Records</div>
+              <div className="text-wrapper-7">History</div>
+              <div className="text-wrapper-8">Settings</div>
+              <div className="text-wrapper-9">Inquiry</div>
+              <div className="text-wrapper-10">Displaying all the contents</div>
+              <img className="data-transfer" alt="Data transfer" src="data-transfer.png" />
+              <img className="web-analytics" alt="Web analytics" src="web-analytics.png" />
+              <img className="money-box" alt="Money box" src="money-box.png" />
+              <img className="database-view" alt="Database view" src="database-view.png" />
+              <img className="time-machine" alt="Time machine" src="time-machine.png" />
+              <img className="settings" alt="Settings" src="settings.png" />
+              <img className="inquiry" alt="Inquiry" src="inquiry.png" />
+              <img className="turismo-turings" alt="Turismo turings" src="turismo-turings-removebg-preview-1.png" />
+              <img className="control-panel" alt="Control panel" src="control-panel.png" />
+            </div>
+            <div className="overlap-3">
+              <div className="payload-sensor">
+                Payload
+                <br />
+                Sensor
+              </div>
+              <img className="menu-vertical" alt="Menu vertical" src="menu-vertical.png" />
+              <div className="text-wrapper-11">452 kg</div>
+              <img className="plus-minus" alt="Plus minus" src="plus-minus-4.png" />
+              <img className="driving" alt="Driving" src="driving.png" />
+            </div>
+            <img className="line" alt="Line" src="line-1.svg" />
+            <div className="overlap-4">
+              <img className="img" alt="Line" src="line-2.svg" />
+              <img className="line-2" alt="Line" src="line-3.svg" />
+              <img className="line-3" alt="Line" src="line-4.svg" />
+              <img className="line-4" alt="Line" src="line-5.svg" />
+              <img className="mask-group" alt="Mask group" src="mask-group-2.png" />
+              <img className="mask-group-2" alt="Mask group" src="mask-group-3.png" />
+              <img className="mask-group-3" alt="Mask group" src="mask-group.png" />
+            </div>
+            <div className="driver-behaviour">
+              Driver
+              <br />
+              Behaviour
+            </div>
+            <div className="text-wrapper-12">G-force</div>
+            <div className="text-wrapper-13">Acceleration</div>
+            <img className="ellipse" alt="Ellipse" src="ellipse-1.svg" />
+            <img className="ellipse-2" alt="Ellipse" src="ellipse-3.svg" />
+            <div className="text-wrapper-14">0</div>
+            <div className="text-wrapper-15">10</div>
+            <div className="text-wrapper-16">20</div>
+            <div className="text-wrapper-17">30</div>
+            <div className="text-wrapper-18">40</div>
+            <div className="text-wrapper-19">9</div>
+            <div className="text-wrapper-20">10</div>
+            <div className="text-wrapper-21">11</div>
+            <div className="text-wrapper-22">12</div>
+          </div>
+          <div className="overlap-5">
+            <div className="velocity-sensor">
+              velocity
+              <br />
+              sensor
+            </div>
+            <img className="menu-vertical-2" alt="Menu vertical" src="menu-vertical-2.png" />
+            <img className="severity" alt="Severity" src="severity.png" />
+            <div className="text-wrapper-23">70 Km/hr</div>
+            <img className="plus-minus-2" alt="Plus minus" src="plus-minus.png" />
+          </div>
+          <div className="text-wrapper-24">Braking</div>
+          <img className="ellipse-3" alt="Ellipse" src="ellipse-2.svg" />
+          <div className="text-wrapper-25">13</div>
+          <div className="text-wrapper-26">14</div>
+          <div className="text-wrapper-27">15</div>
+          <div className="text-wrapper-28">Past 8 hours</div>
+        </div>
+        <div className="overlap-6">
+          <div className="rectangle" />
+          <div className="group">
+            <div className="text-wrapper-29">Engine RPM</div>
+          </div>
+          <div className="text-wrapper-30">Fuel consumption</div>
+          <div className="text-wrapper-31">Engine Temperature</div>
+          <div className="text-wrapper-32">Exhaust Emissions</div>
+          <div className="text-wrapper-33">-</div>
+          <div className="text-wrapper-34">-</div>
+          <div className="text-wrapper-35">-</div>
+          <div className="text-wrapper-36">-</div>
+          <img className="rectangle-2" alt="Rectangle" src="rectangle-19.svg" />
+          <img className="rectangle-3" alt="Rectangle" src="rectangle-21.svg" />
+          <img className="rectangle-4" alt="Rectangle" src="rectangle-22.svg" />
+          <img className="rectangle-5" alt="Rectangle" src="rectangle-23.svg" />
+          <img className="rectangle-6" alt="Rectangle" src="rectangle-24.svg" />
+          <img className="rectangle-7" alt="Rectangle" src="rectangle-25.svg" />
+          <img className="rectangle-8" alt="Rectangle" src="rectangle-26.svg" />
+          <img className="rectangle-9" alt="Rectangle" src="rectangle-27.svg" />
+          <img className="rectangle-10" alt="Rectangle" src="rectangle-28.svg" />
+          <img className="rectangle-11" alt="Rectangle" src="rectangle-29.svg" />
+          <div className="rectangle-12" />
+          <div className="rectangle-13" />
+          <div className="rectangle-14" />
+          <div className="rectangle-15" />
+          <div className="rectangle-16" />
+          <div className="rectangle-17" />
+          <img className="male-user" alt="Male user" src="male-user.png" />
+          <img className="doorbell" alt="Doorbell" src="doorbell.png" />
+          <div className="text-wrapper-37">Engine Perfomance</div>
+          <div className="text-wrapper-38">RPM</div>
+          <div className="text-wrapper-39">Km/L</div>
+          <div className="text-wrapper-40">CO2 - g/km</div>
+          <div className="text-wrapper-41">TN-74 AB-1208</div>
+          <div className="text-wrapper-42">TN-74 AB-1208</div>
+          <div className="text-wrapper-43">TN-74 AB-1208</div>
+          <div className="text-wrapper-44">TN-74 AB-1208</div>
+          <div className="text-wrapper-45">TN-74 AB-1208</div>
+          <div className="text-wrapper-46">TN-74 AB-1208</div>
+          <div className="text-wrapper-47">TN-74 AB-1208</div>
+          <div className="text-wrapper-48">TN-74 AB-1208</div>
+          <div className="text-wrapper-49">TN-74 AB-1208</div>
+          <div className="text-wrapper-50">Fahrenheit</div>
+          <div className="text-wrapper-51">200</div>
+          <div className="text-wrapper-52">100</div>
+          <div className="text-wrapper-53">100</div>
+          <div className="text-wrapper-54">300</div>
+          <img className="engine" alt="Engine" src="engine.png" />
+          <img className="fire" alt="Fire" src="fire.png" />
+          <img className="co" alt="Co" src="co2.png" />
+          <img className="fahrenheit-symbol" alt="Fahrenheit symbol" src="fahrenheit-symbol.png" />
+          <div className="drivers-list">
+            <br />
+            Drivers List
+          </div>
+          <div className="rectangle-18" />
+          <img className="tune" alt="Tune" src="tune.png" />
+          <div className="rectangle-19" />
+          <div className="text-wrapper-55">Driver 1</div>
+          <div className="text-wrapper-56">Driver 2</div>
+          <div className="text-wrapper-57">Driver 3</div>
+          <div className="text-wrapper-58">Driver 4</div>
+          <div className="text-wrapper-59">Driver 5</div>
+          <div className="text-wrapper-60">Driver 6</div>
+          <div className="text-wrapper-61">Driver 7</div>
+          <div className="text-wrapper-62">Driver 8</div>
+          <div className="text-wrapper-63">Driver 9</div>
+          <div className="group-2">
+            <div className="overlap-group-2">
+              <div className="ellipse-4" />
+              <img className="moon-symbol" alt="Moon symbol" src="moon-symbol.png" />
+            </div>
+            <img className="sun" alt="Sun" src="sun.png" />
+          </div>
+          <img className="element-truck" alt="Element truck" src="3d-truck-4.png" />
+          <img className="element-d-truck" alt="Element truck" src="3d-truck-5.png" />
+          <img className="element-truck-2" alt="Element truck" src="3d-truck-6.png" />
+          <img className="element-truck-3" alt="Element truck" src="3d-truck-7.png" />
+          <img className="element-truck-4" alt="Element truck" src="3d-truck-8.png" />
+          <img className="element-truck-5" alt="Element truck" src="3d-truck.png" />
+          <img className="element-truck-6" alt="Element truck" src="image.png" />
+          <img className="element-truck-7" alt="Element truck" src="3d-truck-2.png" />
+          <img className="element-truck-8" alt="Element truck" src="3d-truck-3.png" />
+        </div>
+        <div className="overlap-7">
+          <div className="gyroscope-sensor">
+            Gyroscope
+            <br />
+            sensor
+          </div>
+          <img className="menu-vertical-3" alt="Menu vertical" src="menu-vertical-4.png" />
+          <img className="gyroscope" alt="Gyroscope" src="gyroscope.png" />
+          <div className="text-wrapper-64">24 RPS</div>
+          <img className="plus-minus-3" alt="Plus minus" src="plus-minus-3.png" />
+        </div>
+        <div className="overlap-8">
+          <div className="accelerometer-sensor">
+            Accelerometer
+            <br />
+            sensor
+          </div>
+          <img className="menu-vertical-4" alt="Menu vertical" src="menu-vertical-3.png" />
+          <img className="speedometer" alt="Speedometer" src="speedometer.png" />
+          <div className="text-wrapper-65">100 M/S</div>
+          <img className="plus-minus-4" alt="Plus minus" src="plus-minus-2.png" />
+        </div>
+        <div className="overlap-9">
+          <img className="rectangle-20" alt="Rectangle" src="rectangle-7.png" />
+          <img className="container-truck" alt="Container truck" src="container-truck.png" />
+        </div>
+        <div className="overlap-10">
+          <img className="search" alt="Search" src="search.png" />
+          <div className="text-wrapper-66">Search</div>
+        </div>
+      </div>
+    </div>
+  );
+};
